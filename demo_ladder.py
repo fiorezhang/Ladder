43cb4bbfec67edc943f1a94e271aa2fad3aad5e8
fiorezhang <fiorezhang@sina.com>
2018-08-10 21:24:58 +0800

    add man animation, add sky and river

--- demo_ladder.py
+++ demo_ladder.py
@@ -28,15 +28,29 @@ WINDOW_HEIGHT = 480
 
 MAIN_TOP = WINDOW_HEIGHT//6
 MAIN_BOT = WINDOW_HEIGHT-WINDOW_HEIGHT//6
-MAN_CENTER = WINDOW_WIDTH*3//8
+
+ROCK_HEIGHT = WINDOW_HEIGHT//6
+
 MAN_HEIGHT = WINDOW_HEIGHT//6
-MAN_TOP = MAIN_BOT-MAN_HEIGHT
-MAN_BOT = MAIN_BOT
+MAN_WIDTH = MAN_HEIGHT//2
+MAN_LEFT = WINDOW_WIDTH*3//8
+MAN_BOT = MAIN_BOT-ROCK_HEIGHT
+MAN_TOP = MAN_BOT-MAN_HEIGHT
+MAN_DROP_SPEED = 5
+
 
 CLOUD_HEIGHT = MAIN_TOP
 CLOUD_WIDTH = CLOUD_HEIGHT*4//3
-CLOUD_CNT = 3
+CLOUD_CNT = 4
 CLOUD_SPEED = 1
+
+WAVE_HEIGHT = WINDOW_HEIGHT-MAIN_BOT
+WAVE_WIDTH = WAVE_HEIGHT*3
+WAVE_CNT = 3
+WAVE_SPEED = 5
+
+RIVER_HEIGHT_OVERLAP = WINDOW_HEIGHT//12
+
 
 #RGB
 BLACK       = (  0,   0,   0)
@@ -52,16 +66,17 @@ LIGHTBLUE   = ( 40,  40, 195)
 RED         = (155,   0,   0)
 LIGHTRED    = (195,  40,  40)
 
+COLOR_SKY   = (180, 255, 255)
+COLOR_RIVER = (200, 220, 255)
 
 def main():
-    global exit, fps_lock, display_surf, basic_font
+    global fps_lock, display_surf, basic_font
 
     pygame.init()
     fps_lock = pygame.time.Clock()
     display_surf = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     basic_font = pygame.font.Font('freesansbold.ttf', 20)
     pygame.display.set_caption('Ladder')
-    exit = False
 
     showStartScreen()
     while True:
@@ -81,16 +96,22 @@ def runGame():
     drop_ladder = False
 
     clouds = initialCloud()
+    waves = initialWave()
+    man = initialMan()
     
     while True:
         checkForQuit()
 
         clouds = updateCloud(clouds, move)
+        waves = updateWave(waves, move)
+        man = updateMan(man, move, drop_man)
         
-        display_surf.fill(BLACK)
+        display_surf.fill(WHITE)
+        drawSky()
+        drawRiver()
         drawCloud(clouds)
-        drawWave(move)
-        drawMan(move, drop_man)
+        drawWave(waves)
+        drawMan(man)
         drawRock(move)
         drawLadder(drop_ladder)
 
@@ -106,13 +127,22 @@ def checkForQuit():
                 terminate()
 
 def terminate():
-    exit = True
     pygame.quit()
     sys.exit()
 
+    
+    
+def drawSky():
+    sky = pygame.Rect(0, 0, WINDOW_WIDTH, CLOUD_HEIGHT)
+    pygame.draw.rect(display_surf, COLOR_SKY, sky)
+
+def drawRiver():
+    river = pygame.Rect(0, MAIN_BOT - RIVER_HEIGHT_OVERLAP, WINDOW_WIDTH, WAVE_HEIGHT+RIVER_HEIGHT_OVERLAP)
+    pygame.draw.rect(display_surf, COLOR_RIVER, river)
+    
 def initialCloud():
     clouds = []
-    pos = np.random.randint(0, high=WINDOW_WIDTH-CLOUD_WIDTH*CLOUD_CNT, size=(CLOUD_CNT))
+    pos = np.random.randint(0, high=WINDOW_WIDTH-CLOUD_WIDTH*(CLOUD_CNT-1), size=(CLOUD_CNT))
     pos = np.sort(pos)
     print(pos)
     for i in range(CLOUD_CNT):
@@ -141,11 +171,66 @@ def drawCloud(clouds):
         cloud_img, cloud_pos = cloud[0], cloud[1]
         display_surf.blit(cloud_img, cloud_pos)
 
-def drawWave(move):
-    pass
+def initialWave():
+    waves = []
+    pos = np.random.randint(0, high=WINDOW_WIDTH-WAVE_WIDTH*(WAVE_CNT-1), size=(WAVE_CNT))
+    pos = np.sort(pos)
+    print(pos)
+    for i in range(WAVE_CNT):
+        wave_img = pygame.image.load("resource/wave/wave.png")
+        wave_img = pygame.transform.scale(wave_img, (WAVE_WIDTH, WAVE_HEIGHT))
+        wave_pos = (i*WAVE_WIDTH+pos[i], MAIN_BOT)
+        wave = [wave_img, wave_pos]
+        waves.append(wave)
+    return waves
+    
+def updateWave(waves, move):
+    if move == True:
+        for i, wave in enumerate(waves):
+            wave_img, wave_pos = wave[0], wave[1]
+            wave_pos = (wave_pos[0] - WAVE_SPEED, wave_pos[1])
+            if wave_pos[0] < -WAVE_WIDTH:
+                wave_pos = (WINDOW_WIDTH, wave_pos[1])
+            wave = [wave_img, wave_pos]
+            waves[i] = wave
+    return waves
+        
+def drawWave(waves):
+    for wave in waves:
+        wave_img, wave_pos = wave[0], wave[1]
+        display_surf.blit(wave_img, wave_pos)
 
-def drawMan(move, drop_man):
-    pass
+man_pose_num = 0 #全局变量
+MAN_POSE_CNT = 6
+def initialMan():
+    global man_pose_num
+    man_img = pygame.image.load("resource/man/man"+str(man_pose_num)+".png")
+    man_img = pygame.transform.scale(man_img, (MAN_WIDTH, MAN_HEIGHT))
+    man_pos = (MAN_LEFT, MAN_BOT-MAN_HEIGHT)
+    man = [man_img, man_pos]    
+    return man
+    
+def updateMan(man, move, drop_man):
+    global man_pose_num
+    man_img, man_pos = man[0], man[1]
+    if move == True:
+        man_pose_num += 1
+        if man_pose_num == MAN_POSE_CNT:
+            man_pose_num = 0
+        man_img = pygame.image.load("resource/man/man"+str(man_pose_num)+".png")
+        man_img = pygame.transform.scale(man_img, (MAN_WIDTH, MAN_HEIGHT))
+        man = [man_img, man_pos]
+    elif drop_man == True:
+        man_img = pygame.image.load("resource/man/man"+str(man_pose_num)+".png")
+        man_img = pygame.transform.scale(man_img, (MAN_WIDTH, MAN_HEIGHT))
+        man_pos = (man_pos[0], man_pos[1]+MAN_DROP_SPEED) #碰撞检测在主循环获取man_pos的值后做
+        man = [man_img, man_pos]
+    return man
+        
+def drawMan(man):
+    man_img, man_pos = man[0], man[1]
+    display_surf.blit(man_img, man_pos)
+    
 
 def drawRock(move):
     pass
